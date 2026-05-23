69f7601da95f10912db123779a5e1c73226c7962
Robert Philipp <rob.philipp@gmail.com>
2026-05-23 09:42:08 -0400

    updated the interpolations for the outlier chart

--- src/app/examples/StreamingOutlierChart.tsx
+++ src/app/examples/StreamingOutlierChart.tsx
@@ -85,6 +85,9 @@ const INTERPOLATIONS = new Map<string, [string, d3.CurveFactory]>([
     ['curveLinear', ['Linear', d3.curveLinear]],
     ['curveNatural', ['Natural', d3.curveNatural]],
     ['curveMonotoneX', ['Monotone', d3.curveMonotoneX]],
+    ['curveStep', ['Step', d3.curveStep]],
+    ['curveStepAfter', ['Step After', d3.curveStepAfter]],
+    ['curveStepBefore', ['Step Before', d3.curveStepBefore]],
     ['curveBumpX', ['Bump', d3.curveBumpX]],
 ])
 
