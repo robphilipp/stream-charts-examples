1a2173f5678836d3f1ffab144fc54df3aafc357f
Robert Philipp <rob.philipp@gmail.com>
2026-08-08 12:10:01 -0400

    ocd

--- src/app/examples/appstate/scatterChartStore.ts
+++ src/app/examples/appstate/scatterChartStore.ts
@@ -40,75 +40,44 @@ const initialState: ScatterChartState = {
 }
 
 type ScatterChartStore = ScatterChartState & {
-    // initialData: Array<TimeSeries>
     setInitialData: (initialData: Array<TimeSeries>) => void
-
-    // observable: Observable<TimeSeriesChartData>
-
-    // subscription: Subscription | undefined
     setSubscription: (subscription: Subscription) => void
-
-    // running: boolean
     setRunning: (running: boolean) => void
     startRunning: () => void
     stopRunning: () => void
-
-    // x1axisRange: Range
     setX1axisRange: (range: Range) => void
-    // x2axisRange: Range
     setX2axisRange: (range: Range) => void
-
-    // filterValue: string
     setFilterValue: (filterValue: string) => void
-
-    // visibility: Visibility
     setVisibility: (visibility: Visibility) => void
-
-    // selectedInterpolationName: string
     setSelectedInterpolationName: (name: string) => void
-
-    // dropAfterMs: number
     setDropAfterMs: (dropAfterMs: number) => void
-
-    // reset the state
     reset: () => void
 }
 
 export const useScatterChartStore: UseBoundStore<StoreApi<ScatterChartStore>> =
     create<ScatterChartStore>((set) => ({
         ...initialState,
-        // initialData: [],
         setInitialData: (initialData: Array<TimeSeries>) => set({
             initialData,
             observable: randomDataObservable(initialData)
         }),
 
-        // observable: randomDataObservable([]),
-
-        // subscription: undefined,
         setSubscription: (subscription: Subscription) => set({subscription}),
 
-        // running: false,
         setRunning: running => set({running}),
         startRunning: () => set({running: true}),
         stopRunning: () => set({running: false}),
 
-        // x1axisRange: [0, 10000],
         setX1axisRange: (range: Range) => set({x1axisRange: range}),
-        // x2axisRange: [0, 5000],
         setX2axisRange: (range: Range) => set({x2axisRange: range}),
 
-        // filterValue: '',
         setFilterValue: filterValue => set({filterValue}),
 
         // holds status of the visibility for tooltip, tracker, markers, legend
-        // visibility: createInitialVisibility(),
         setVisibility: visibility => set({visibility}),
 
-        // selectedInterpolationName: 'curveLinear',
         setSelectedInterpolationName: name => set({selectedInterpolationName: name}),
 
-        // dropAfterMs: 0,
         setDropAfterMs: (dropAfterMs: number) => set({dropAfterMs}),
 
         reset: () => set(initialState)
